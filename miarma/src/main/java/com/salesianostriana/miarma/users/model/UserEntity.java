b71f8dd09b390e45e8664075feed450ba5e48402
diana <dianagonzalez4d@gmail.com>
2022-02-14 14:25:11 +0100

    Se indentó la clase modelo de user

--- miarma/src/main/java/com/salesianostriana/miarma/users/model/UserEntity.java
+++ miarma/src/main/java/com/salesianostriana/miarma/users/model/UserEntity.java
@@ -57,8 +57,7 @@ public class UserEntity implements UserDetails {
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
-
-
+    
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
@@ -69,40 +68,21 @@ public class UserEntity implements UserDetails {
         return email;
     }
 
-
-    /**
-     * No vamos a gestionar la expiración de cuentas. De hacerse, se tendría que dar
-     * cuerpo a este método
-     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
-    /**
-     * No vamos a gestionar el bloqueo de cuentas. De hacerse, se tendría que dar
-     * cuerpo a este método
-     */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
-    /**
-     * No vamos a gestionar la expiración de cuentas. De hacerse, se tendría que dar
-     * cuerpo a este método
-     */
 
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
-
-    /**
-     * No vamos a gestionar el bloqueo de cuentas. De hacerse, se tendría que dar
-     * cuerpo a este método
-     */
     @Override
     public boolean isEnabled() {
         return true;
